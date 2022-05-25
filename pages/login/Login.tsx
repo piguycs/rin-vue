37c216232136d0dc9a72585407c96943b8a3ee43
thepiguy <bhubhubarks20@gmail.com>
2022-05-25 22:18:37 +0200

    better login page

--- pages/login/Login.tsx
+++ pages/login/Login.tsx
@@ -1,36 +1,120 @@
 import styles from "../../styles/Login.module.css";
-import inputStyles from "../../styles/FancyInputBoxes.module.scss"
-import { FormEvent, useRef } from "react";
+import inputStyles from "../../styles/FancyInputBoxes.module.scss";
+import { FormEvent, useEffect, useRef, useState } from "react";
 
 export default function Login() {
-  const psswdincorrect = useRef<HTMLSpanElement>(null)
-  const unameincorrect = useRef<HTMLSpanElement>(null)
+  const [showPsswd, togglePsswd] = useState<"password" | "text">("password");
+  const [canChangeText, setCCT] = useState(true);
+  const [others, setOthers] = useState(false);
+
+  const textlist = ["start chatting", "connect", "experience the next gen"];
+  const [marketing, setMarketing] = useState(textlist[0]);
+
+  const psswdincorrect = useRef<HTMLSpanElement>(null);
+  const unameincorrect = useRef<HTMLSpanElement>(null);
 
   const submit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    psswdincorrect.current!.style.visibility = "visible"
+    psswdincorrect.current!.style.visibility = "visible";
+  };
+
+  useEffect(() => {
+    if (showPsswd === "text") {
+      setTimeout(() => {
+        togglePsswd("password");
+      }, 5000);
+    }
+  }, [showPsswd]);
+
+  const changeText = (e: any) => {
+    if (canChangeText) {
+      let incriment = -1;
+      if (marketing === textlist[textlist.length - 1]) {
+        incriment = 0;
+      } else {
+        incriment = textlist.indexOf(marketing) + 1;
+      }
+
+      setCCT(false);
+      setTimeout(() => {
+        setCCT(true);
+      }, 700);
+      setMarketing(textlist[incriment]);
+    }
   };
 
   return (
     <div className={styles.loginroot}>
-      <form className={styles.loginbox} onSubmit={submit}>
+      <div className={styles.loginbox}>
+        <h1>
+          Sign In to{" "}
+          <span
+            style={{ color: "hsl(346, 67%, 65%)" }}
+            onPointerEnter={changeText}
+          >
+            {marketing}
+          </span>
+        </h1>
 
-        <label className={inputStyles.input}>
-          <input className={inputStyles.input__field} type="text" placeholder="" />
-          <span className={inputStyles.input__label}>Username</span>
-          <span ref={unameincorrect} className={inputStyles.inputerror}>Username is incorrect</span>
-        </label>
+        <form className={styles.loginform} onSubmit={submit}>
+          <label className={inputStyles.input}>
+            <input
+              className={inputStyles.input__field}
+              type="text"
+              placeholder=""
+            />
+            <span className={inputStyles.input__label}>Username</span>
+            <span ref={unameincorrect} className={inputStyles.inputerror}>
+              Username is incorrect
+            </span>
+          </label>
 
-        <label className={inputStyles.input}>
-          <input className={inputStyles.input__field} type="text" placeholder="" />
-          <span className={inputStyles.input__label}>Password</span>
-          <input type="checkbox" className={inputStyles.showpsswd} />
-          <span ref={psswdincorrect} className={inputStyles.inputerror}>Password is incorrect</span>
-        </label>
-    
-        <input className={inputStyles.submit} type="submit" value="Login" />
+          <label className={inputStyles.input}>
+            <input
+              className={inputStyles.input__field}
+              type={showPsswd}
+              placeholder=""
+            />
+            <span className={inputStyles.input__label}>Password</span>
+            <span
+              className={inputStyles.showpsswd}
+              style={showPsswd === "password" ? {color: "hsl(0, 0%, 43%)"} : {}}
+              onClick={() => {
+                if (showPsswd === "password") {
+                  togglePsswd("text");
+                } else {
+                  togglePsswd("password");
+                }
+              }}
+            >
+              {showPsswd == "password" ? "﯏" : "﯎"}
+            </span>
+            <span ref={psswdincorrect} className={inputStyles.inputerror}>
+              Password is incorrect
+            </span>
+          </label>
 
-      </form>
+          <label>
+            <input
+              className={inputStyles.submit}
+              type="submit"
+              value="Sign In"
+            />
+          </label>
+        </form>
+        <span
+          className={styles.extraslink}
+          onClick={() => {
+            setOthers(!others);
+          }}
+        >
+          more options
+        </span>
+      </div>
+
+      {others ? (
+        <div className={styles.authbox}>Metamask, Github, Gitlab, Google</div>
+      ) : null}
     </div>
   );
 }
