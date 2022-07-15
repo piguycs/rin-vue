d6cc70085cba491356241d7b4df634118a2b92cd
RocKing1001 <bhubhubarks20@gmail.com>
2022-07-15 20:42:25 +0530

    Added the plugin file which was deleted last time

---
+++ plugins/firebasePlugin.client.ts
@@ -0,0 +1,27 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "@firebase/firestore";
+
+export default defineNuxtPlugin((nuxtApp) => {
+  const config = useRuntimeConfig();
+
+  const firebaseConfig = {
+    apiKey: config.APIKEY,
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  
+  initUser()
+
+  const auth = getAuth(app)
+  const firestore = getFirestore(app)
+
+  return {
+    provide: {
+      auth,
+      firestore
+    }
+  }
+
+});
